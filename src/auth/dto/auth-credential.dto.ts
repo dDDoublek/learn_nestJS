0231c6fe33376553e5e8dae240b13e8e5ecf621e
hankyeolk <2247han@gmail.com>
2022-02-23 21:18:30 +0900

    220222 06-auth doing

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,15 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(8)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: '비밀번호는 8자리 이상이고, 영어와 숫자의 조합입니다.',
+  })
   password: string;
 }
