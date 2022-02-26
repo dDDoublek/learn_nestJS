f9e9b0364261c79aaeb7d869f6ebada23a968e44
hankyeolk <2247han@gmail.com>
2022-02-26 18:58:28 +0900

    220226 06-auth doing, jwt setting

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { compare } from 'bcryptjs';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
@@ -8,18 +9,23 @@ import { UserRepository } from './user.repository';
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async singUp(authcredentialDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authcredentialDto);
   }
 
-  async signIn(authcredentialDto: AuthCredentialDto) {
+  async signIn(
+    authcredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authcredentialDto;
     const user = this.userRepository.findOne({ username });
 
     if (user && (await compare(password, (await user).password))) {
-      return 'login success';
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('로그인 정보를 올바르게 입력해주세요.');
     }
