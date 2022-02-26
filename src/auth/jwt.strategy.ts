32a105607d54ab5ccdfddd68e1b9c48f24506285
hankyeolk <2247han@gmail.com>
2022-02-26 18:58:48 +0900

    220226 06-auth doing, passport-jwt strategy done

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  // jwt strategy를 passport에서 기본 규칙으로 사용하기 위해서 모듈을 불러와서 넣어준다.
+
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {
+    super({
+      secretOrKey: 'salt120', // token validation
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // token extract from header
+    });
+  }
+
+  async validate(payload) {
+    const { username } = payload;
+    const user: User = await this.userRepository.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
